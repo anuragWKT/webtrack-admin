7d43087777fdc5a9e7f07c7ebe3dd270e9100f2c
anuragWKT <anurag.s@webknot.in>
2026-01-31 20:16:41 +0530

    initial commit

---
+++ src/main/java/com/webknot/webtrak_admin/WebtrakAdminApplication.java
@@ -0,0 +1,13 @@
+package com.webknot.webtrak_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtrakAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtrakAdminApplication.class, args);
+	}
+
+}
